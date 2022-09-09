7953f10d70426adc6420bf39ff8376422955f04b
david.schkalee <david.schkalee@sportalliance.com>
2022-09-09 19:04:15 +0200

    feat: add static `withObservableOverrides`

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,50 @@
-import { action, makeAutoObservable } from "mobx";
-import * as pkg from "wrap-request";
-
-export * from "wrap-request";
+import { action, AnnotationsMap, makeAutoObservable } from 'mobx';
+import * as pkg from 'wrap-request';
 
 const originalWrapRequest = pkg.wrapRequest;
 
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
 type WrapRequestParams = Parameters<typeof originalWrapRequest>;
+type RequestFn = WrapRequestParams[0];
+type RequestOptions = Parameters<RequestFn>[1];
+type Options = WrapRequestParams[1];
 
-(pkg as any).wrapRequest = function (...args: WrapRequestParams) {
-  const res = originalWrapRequest(...args);
+export { RequestFn, Options, RequestOptions };
 
-  makeAutoObservable(res, {
-    reset: action,
-    request: action
-  });
+function buildNewWrapRequest(
+    overrides: AnnotationsMap<pkg.WrapRequest, NoInfer<PropertyKey>>,
+    ...args: WrapRequestParams
+) {
+    const res = originalWrapRequest(...args);
 
-  return res;
-};
+    makeAutoObservable(res, overrides);
+
+    return res;
+}
+
+function newWrapRequest(
+    ...args: WrapRequestParams
+): ReturnType<typeof buildNewWrapRequest> {
+    return buildNewWrapRequest(
+        {
+            reset: action,
+            request: action,
+        },
+        ...args
+    );
+}
+
+function withObservableOverrides(
+    overrides: AnnotationsMap<pkg.WrapRequest, NoInfer<PropertyKey>>
+) {
+    return (...args: WrapRequestParams) => {
+        return buildNewWrapRequest(overrides, ...args);
+    };
+}
+
+newWrapRequest.withObservableOverrides = withObservableOverrides;
+
+(pkg as any).wrapRequest = newWrapRequest;
+
+export { newWrapRequest as wrapRequest };
