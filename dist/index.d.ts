339680509c68d56c2a5503a10275d437767836bb
david.schkalee <david.schkalee@sportalliance.com>
2022-09-09 19:09:06 +0200

    chore: compile code

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,15 @@
-export * from "wrap-request";
+import { AnnotationsMap } from 'mobx';
+import * as pkg from 'wrap-request';
+declare const originalWrapRequest: typeof pkg.wrapRequest;
+declare type NoInfer<T> = [T][T extends any ? 0 : never];
+declare type WrapRequestParams = Parameters<typeof originalWrapRequest>;
+declare type RequestFn = WrapRequestParams[0];
+declare type RequestOptions = Parameters<RequestFn>[1];
+declare type Options = WrapRequestParams[1];
+export { RequestFn, Options, RequestOptions };
+declare function buildNewWrapRequest(overrides: AnnotationsMap<pkg.WrapRequest, NoInfer<PropertyKey>>, ...args: WrapRequestParams): pkg.WrapRequest<unknown, unknown, unknown, unknown>;
+declare function newWrapRequest(...args: WrapRequestParams): ReturnType<typeof buildNewWrapRequest>;
+declare namespace newWrapRequest {
+    var withObservableOverrides: (overrides: AnnotationsMap<pkg.WrapRequest<any, any, any, any>, string | number | symbol>) => (request: pkg.RequestFn<unknown, unknown>, options?: pkg.Options<unknown, unknown, unknown> | undefined) => pkg.WrapRequest<unknown, unknown, unknown, unknown>;
+}
+export { newWrapRequest as wrapRequest };
--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,26 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.wrapRequest = void 0;
 const mobx_1 = require("mobx");
 const pkg = require("wrap-request");
-__exportStar(require("wrap-request"), exports);
 const originalWrapRequest = pkg.wrapRequest;
-pkg.wrapRequest = function (...args) {
+function buildNewWrapRequest(overrides, ...args) {
     const res = originalWrapRequest(...args);
-    mobx_1.makeAutoObservable(res, {
-        reset: mobx_1.action,
-        request: mobx_1.action
-    });
+    mobx_1.makeAutoObservable(res, overrides);
     return res;
-};
+}
+function newWrapRequest(...args) {
+    return buildNewWrapRequest({
+        reset: mobx_1.action,
+        request: mobx_1.action,
+    }, ...args);
+}
+exports.wrapRequest = newWrapRequest;
+function withObservableOverrides(overrides) {
+    return (...args) => {
+        return buildNewWrapRequest(overrides, ...args);
+    };
+}
+newWrapRequest.withObservableOverrides = withObservableOverrides;
+pkg.wrapRequest = newWrapRequest;
 //# sourceMappingURL=index.js.map
