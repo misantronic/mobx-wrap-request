b7a9eed5aaf58f3e85de9d8ecd09f364a3e6db9e
david.schkalee <david.schkalee@sportalliance.com>
2022-09-30 11:42:36 +0200

    chore: update types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,6 +4,6 @@ declare const originalWrapRequest: typeof pkg.wrapRequest;
 declare type WrapRequestParams<$, P = any, $$ = $, MD = any> = Parameters<typeof originalWrapRequest<$, P, $$, MD>>;
 declare function newWrapRequest<$, P = any, $$ = $, MD = any>(...args: WrapRequestParams<$, P, $$, MD>): pkg.WrapRequest<$, P, $$, MD>;
 declare namespace newWrapRequest {
-    var withObservableOverrides: (overrides: AnnotationsMap<pkg.WrapRequest<any, any, any, any>, PropertyKey>) => <$, P = any, $$ = $, MD = any>(request: pkg.RequestFn<$, P>, options?: pkg.Options<$, $$, MD> | undefined) => pkg.WrapRequest<$, P, $$, MD>;
+    var withObservableOverrides: (overrides: AnnotationsMap<pkg.WrapRequest<any, any, any, any>, PropertyKey>) => <$, P = any, $$ = $, MD = any>(request: pkg.RequestFn<$, P>, options?: pkg.Options<$, MD> | undefined) => pkg.WrapRequest<$, P, $$, MD>;
 }
 export { newWrapRequest as wrapRequest };
